eab3f28266273d381efef5c6b3775ba083f96ef2
KatianaAlmeida <Katianaalmeida48@gmail.com>
2024-09-01 20:57:13 +0200

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,3 @@
-const sendMail = document.getElementById('sendMail');
-sendMail.addEventListener('click', function () {
-  const email = document.getElementById('email').value.trim();
-  let status_message = document.getElementById('status_message');
-
-  // check if email is valid
-  if (!validateEmail(email)) {
-    status_message.classList.remove('sent');
-    status_message.classList.add('invalid');
-    document.querySelector('.invalid').innerHTML = 'Email is not valid.';
-  }else{
-    // send email with emailJS
-    let params = {
-      from_name: document.getElementById("fullName").value,
-      email_id: document.getElementById("email").value,
-      subject: document.getElementById("subject").value,
-      message: document.getElementById("message").value
-    }
-    emailjs.send("service_g7o7kzv", "template_7lh3d93", params).then(function (res) {
-      status_message.classList.remove('invalid');
-      status_message.classList.add('sent');
-      document.querySelector('.sent').innerHTML = 'Message Send!';
-    });
-    document.getElementById("fullName").value = '';
-    document.getElementById("email").value = '';
-    document.getElementById("subject").value = '';
-    document.getElementById("message").value = '';
-  }
-
-});
-
-function validateEmail(email) {
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailPattern.test(String(email).toLowerCase());
-}
-
 function showSidebar(toggle){
   const frontbarElement = document.querySelector('.links2');
   const menu_button = document.querySelector('.menu_button');
